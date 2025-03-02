7a222b6f82cb9799e201943aca14510a12a95de5
Pascal Bawidamann <paba@netlight.com>
2025-03-02 21:46:57 +0100

    Added checkboxes and fixed styling

--- src/routes/create-event/+page.svelte
+++ src/routes/create-event/+page.svelte
@@ -4,61 +4,90 @@
 	import { isCalendarEvent } from '$lib/types/component-types';
 	import { addCalendarEvent } from '../eventStore.svelte';
 	let newEvent: Partial<CalendarEvent> = {};
-    let startTime: String = "";
-    let endTime: String = "";
+	let startTime: String = '';
+	let endTime: String = '';
 
-    const convertStringToDate = (timeString: String) =>  {
-        const [hours, minutes] = timeString.split(":");
-        let newDate = new Date();
-        newDate.setHours(Number(hours), Number(minutes));
-        return newDate;
-    }
+	const convertStringToDate = (timeString: String) => {
+		const [hours, minutes] = timeString.split(':');
+		let newDate = new Date();
+		newDate.setHours(Number(hours), Number(minutes));
+		return newDate;
+	};
 
 	const saveEvent = (newEvent: Partial<CalendarEvent>) => {
 		const saveableEvent: Partial<CalendarEvent> = {
 			pushNotification: false,
 			reoccuring: false,
 			...newEvent,
-            startTime:  convertStringToDate(startTime),
-            endTime:  convertStringToDate(endTime)
+			startTime: convertStringToDate(startTime),
+			endTime: convertStringToDate(endTime)
 		};
 
-
-        console.log(saveableEvent, isCalendarEvent(saveableEvent))
+		console.log(saveableEvent, isCalendarEvent(saveableEvent));
 
 		if (isCalendarEvent(saveableEvent)) {
 			addCalendarEvent(saveableEvent);
-            goto('/');
+			goto('/');
 		}
 	};
 </script>
 
-<input class="w-full" type="text" placeholder="Add Something" bind:value={newEvent.name} />
-<input
-	class="w-fit" 
-	id="appointment-time"
-	type="time"
-	name="appointment-time"
-	bind:value={startTime}
-/>
-<input
-	class="w-fit"
-	id="appointment-time"
-	type="time"
-	name="appointment-time"
-	bind:value={endTime}
-/>
+<div class="container">
+	<button class="back m-2" on:click={() => goto('/')}>back</button>
+	<div class="w-full p-2">
+		<input class="name-input" type="text" placeholder="Add Something" bind:value={newEvent.name} />
+	</div>
+	<input
+		class="m-2 w-fit"
+		id="appointment-time"
+		type="time"
+		name="appointment-time"
+		bind:value={startTime}
+	/>
+	<input
+		class="w-fit"
+		id="appointment-time"
+		type="time"
+		name="appointment-time"
+		bind:value={endTime}
+	/>
 
-<button class="submit" on:click={() => saveEvent(newEvent)}>✓</button>
+	<div class="checkbox">
+		<label>
+			<input type="checkbox" id="exampleCheckbox" bind:checked={newEvent.pushNotification}/>
+			Send Notification
+		</label>
+	</div>
+
+	<div class="checkbox">
+		<label>
+			<input type="checkbox" id="exampleCheckbox" bind:checked={newEvent.reoccuring}/>
+			Is reoccuring 
+		</label>
+	</div>
+
+	<button class="submit" on:click={() => saveEvent(newEvent)}>✓</button>
+</div>
 
 <style>
+	.name-input {
+		width: 100%;
+	}
+
+	.checkbox {
+		width: fit-content;
+		margin: 0.3rem 0 0.6rem 0.5rem;
+		padding: 0.5rem 0.5rem 0.5rem 0.3rem;
+		background: var(--input-bg);
+		border: 1px solid var(--input-border);
+		border-radius: 8px;
+		color: var(--text-color);
+	}
+
 	input[type='time'],
-	input[type='text'],
-	select,
-	input[type='color'] {
+	input[type='text'] {
 		padding: 0.6rem;
 		background: var(--input-bg);
-		margin-left: 0.3rem;
 		border: 1px solid var(--input-border);
 		border-radius: 8px;
 		color: var(--text-color);
@@ -66,8 +95,14 @@
 		font-size: 1rem;
 	}
 
+	.back {
+		padding: 0.4rem 1rem;
+		background: var(--input-bg);
+		border: 1px solid var(--input-border);
+		margin-bottom: 2rem;
+	}
+
 	button {
-		margin-left: 0.5rem;
 		padding: 0.6rem 1rem;
 		background: var(--accent-blue);
 		color: white;
@@ -84,8 +119,7 @@
 		right: 2rem;
 	}
 
-	input[type='text']:focus,
-	select:focus {
+	input[type='text']:focus {
 		border-color: var(--accent-blue);
 	}
 </style>
